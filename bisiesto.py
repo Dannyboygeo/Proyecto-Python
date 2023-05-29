9896573736e774fe8aa4130a749b685e1d3e2a37
Dannyboygeo <134558718+Dannyboygeo@users.noreply.github.com>
2023-05-29 04:00:48 +0200

    bisiesto.py

--- bisiesto.py
+++ bisiesto.py
@@ -7,7 +7,7 @@ Created on Sun May 28 07:12:53 2023
 
 anio=int(input('Ingrese el anio a convertir: '))
 if anio%4==0 and anio%400==0:
-    print('El anio',anio,'es bisiesto')
+    print('Verdadero')
 else:
-    print('El anio',anio,'no es bisiesto. Ingrese otro valor')
+    print('Falso')
         
